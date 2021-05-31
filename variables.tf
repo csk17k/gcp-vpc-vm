219edfce7c8e46aa2db53417c686b202cff6e021
csk17k <84774760+csk17k@users.noreply.github.com>
2021-05-31 14:36:19 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -49,32 +49,3 @@ variable "placeholder" {
   default     = "placekitten.com"
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
-variable "env" {
-  type = "string"
-}
-
-variable "image_tag" {
-  type = "string"
-}
-
-variable "k8s_master_allowed_ip" {
-  type = "string"
-}
-
-variable "machine_type" {
-  default = "n1-standard-1"
-  type    = "string"
-}
-
-variable "network_name" {
-  type = "string"
-}
-
-variable "node_count" {
-  default = "1"
-  type    = "string"
-}
-
-variable "region" {
-  type = "string"
-}
