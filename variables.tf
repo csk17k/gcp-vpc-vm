51e2a1a0da0790a6608af7fb7fa55f2b25ce9be0
csk17k <84774760+csk17k@users.noreply.github.com>
2021-05-31 14:36:54 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -35,6 +35,37 @@ variable "machine_type" {
   default     = "f1-micro"
 }
 
+variable "env" {
+  type = "string"
+}
+
+variable "image_tag" {
+  type = "string"
+}
+
+variable "k8s_master_allowed_ip" {
+  type = "string"
+}
+
+variable "machine_type" {
+  default = "n1-standard-1"
+  type    = "string"
+}
+
+variable "network_name" {
+  type = "string"
+}
+
+variable "node_count" {
+  default = "1"
+  type    = "string"
+}
+
+variable "region" {
+  type = "string"
+}
+
+
 variable "height" {
   default     = "400"
   description = "Image height in pixels."
