6cb858aa1b8c8a9a1f39376b398d0919774e227e
csk17k <84774760+csk17k@users.noreply.github.com>
2021-05-31 14:43:14 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -40,24 +40,19 @@ variable "env" {
 }
 
 variable "image_tag" {
-  type = string
 }
 
 variable "k8s_master_allowed_ip" {
-  type = string
 }
 
 variable "machine_type" {
-  type    = string
 }
 
 variable "network_name" {
-  type = string
 }
 
 variable "node_count" {
   default = "1"
-  type    = string
 }
 
 
